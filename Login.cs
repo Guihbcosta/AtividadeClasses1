7b4bf7a35832cbba90e4b0c1558b455de4ea28e9
Guihbcosta <54807958+Guihbcosta@users.noreply.github.com>
2019-09-02 18:43:35 -0300

    Add files via upload
    classes

---
+++ Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlameOfficeGuilhermeHenrique1705709.Classes
+{
+    public class Login
+    {
+        public int id { get; set; }
+        public string login { get; set; }
+        public string senha { get; set; }
+
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlameOfficeGuilhermeHenrique1705709.Classes
+{
+    public class Usuario : Login
+    {
+
+        public string nome { get; set; }
+
+        public List<object> GerarLista()
+        {
+
+            List<object> listaUsuario = new List<object>();
+            return listaUsuario;
+        }
+    }
+}
